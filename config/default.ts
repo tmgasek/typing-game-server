127907f0c2fbe0b160706be177a9a1cfc23b8e21
tmgasek <tomaszgasek28@gmail.com>
2023-01-06 13:48:32 +0000

    change evs

--- config/default.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  corsOrigin: "*",
-  port: 3001,
-  host: "localhost",
-};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import config from "config";
+// import config from "config";
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
@@ -6,9 +6,13 @@ import { version } from "../package.json";
 import socket from "./socket";
 import logger from "./utils/logger";
 
-const port = config.get<number>("port");
-const host = config.get<string>("host");
-const corsOrigin = config.get<string>("corsOrigin");
+// const port = config.get<number>("port");
+// const host = config.get<string>("host");
+// const corsOrigin = config.get<string>("corsOrigin");
+
+const port = process.env.PORT || 3001;
+const host = process.env.HOST || "localhost";
+const corsOrigin = process.env.CORS_ORIGIN || "*";
 
 const app = express();
 const httpServer = createServer(app);
@@ -23,7 +27,7 @@ app.get("/", (_, res) => {
   res.send("server up");
 });
 
-httpServer.listen(port, host, () => {
+httpServer.listen(port, () => {
   logger.info(`Server version ${version} is listening`);
   logger.info(`http://${host}:${port}`);
   socket({ io });
